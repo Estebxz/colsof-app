93af1165d26dae611aebac33831633a3bfe72218
Estebxz <estebanmendezm1995@gmail.com>
2026-02-27 19:27:16 -0500

    refactor: cambiar comillas simples a dobles en tipos, permitir roles personalizados en Role, y eliminar duplicado 'spinner' en IconName

--- app/types/types.ts
+++ app/types/types.ts
@@ -6,30 +6,30 @@ export type LoginUserProps = {
   rol?: Role;
 };
 
-export type Role = 'Administrador' | 'Gestor' | 'Tecnico';
+export type Role = "Administrador" | "Gestor" | "Tecnico" | (string & {});
 
-export type IconName = 
-  | 'arrow-left'
-  | 'arrow-right'
-  | 'grid'
-  | 'github'
-  | 'escape'
-  | 'linkedin'
-  | 'panel-left'
-  | 'panel-right'
-  | 'plus'
-  | 'spinner'
-  | 'archive-box'
-  | 'trash'
-  | 'donut'
-  | 'arrow-up-left'
-  | 'alert-circle'
-  | 'eye-rounded'
-  | 'home'
-  | 'charts'
-  | 'db'
-  | 'user'
-  | 'camera'
-  | 'bell'
-  | 'terminal'
-  | 'spinner';
+export type IconName =
+  | "arrow-left"
+  | "arrow-right"
+  | "grid"
+  | "github"
+  | "escape"
+  | "linkedin"
+  | "panel-left"
+  | "panel-right"
+  | "plus"
+  | "spinner"
+  | "archive-box"
+  | "trash"
+  | "donut"
+  | "arrow-up-left"
+  | "alert-circle"
+  | "eye-rounded"
+  | "home"
+  | "charts"
+  | "db"
+  | "user"
+  | "camera"
+  | "bell"
+  | "terminal"
+  | "spinner";
